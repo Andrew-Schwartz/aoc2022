5910fb0803d2270ede03e2fe24928ef1fb351191
Andrew-Schwartz <andrewschwartz59@gmail.com>
2022-12-06 01:54:30 -0500

    day6 const

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,6 @@
 use aoc_runner_derive::aoc;
 
+/*
 fn unique<const N: usize>(arr: &[u8; N]) -> bool {
     let mut bits = 0_u32;
     for &char in arr {
@@ -15,18 +16,36 @@ fn solve<const N: usize>(input: &[u8]) -> usize {
         .position(unique)
         .unwrap() + N
 }
-
-#[aoc(day6, part1)]
-fn part1(input: &[u8]) -> usize {
-    solve::<4>(input)
-}
-
-#[aoc(day6, part2)]
-fn part2(input: &[u8]) -> usize {
-    solve::<14>(input)
-}
+*/
 
 const fn const_solve<const N: usize>() -> usize {
     let input = include_bytes!("../input/2022/day6.txt");
 
+    let mut i = 0;
+    loop {
+        let mut bits = 0_u32;
+
+        let mut j = 0;
+        while j < N {
+            let bit = 1 << (input[i + j] - b'a');
+            if bits & bit != 0 { break }
+            bits |= bit;
+            j += 1;
+        }
+        if j == N { return i + N }
+
+        i += 1;
+    }
+}
+
+#[aoc(day6, part1)]
+fn part1(_: &[u8]) -> usize {
+    const SOLUTION: usize = const_solve::<4>();
+    SOLUTION
+}
+
+#[aoc(day6, part2)]
+fn part2(_: &[u8]) -> usize {
+    const SOLUTION: usize = const_solve::<14>();
+    SOLUTION
 }
